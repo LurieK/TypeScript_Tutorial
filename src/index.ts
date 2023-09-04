eaf9bde290e02c02bb0b7154c726130aecb953ff
Lurie <lurainekimmerle@gmail.com>
2023-09-04 09:23:49 +0000

    set object type definitions in index.ts.
    removed and cleared out unneeded files

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,64 @@
-const returningUserDisplay = document.querySelector("#returning-user")!;
-const userNameDisplay = document.querySelector("#user")!;
-const reviewTotalDisplay = document.querySelector("#reviews")!;
+const returningUserDisplay = document.querySelector('#returning-user')!
+const userNameDisplay = document.querySelector('#user')!
+const reviewTotalDisplay = document.querySelector('#reviews')!
 
-let isOpen: boolean;
+let isOpen: boolean
 
-const reviews = [
-  {
-    name: "Sheia",
-    stars: 5,
-    loyaltyUser: true,
-    date: "01-04-2021"
-  },
-  {
-    name: "Andrzej",
-    stars: 3,
-    loyaltyUser: false,
-    date: "28-03-2021"
-  },
-  {
-    name: "Omar",
-    stars: 4,
-    loyaltyUser: true,
-    date: "27-03-2021"
-  }
-];
+const reviews: {
+    name: string;
+    stars: number;
+    loyaltyUser: boolean;
+    date: string;
+}[] = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
 
 function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-  const iconDisplay = isLoyalty ? "⭐" : "";
-  reviewTotalDisplay.innerHTML =
-    "review total " +
-    value.toString() +
-    "| last reviewed by " +
-    reviewer +
-    " " +
-    iconDisplay;
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
-const you = {
-  userName: { firstName: "Bobby", lastName: "Brown" },
-  isReturning: "yes"
-};
 
-function populateUser(isReturning: boolean, userName: string) {
-  if (isReturning) {
-    returningUserDisplay.innerHTML = "back";
-  }
-  userNameDisplay.innerHTML = userName.toString();
+const you: {
+    userName : {firstName: string, lastName: string};
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[];
+} = {
+   userName: {firstName: 'Bobby', lastName: "Smith"},
+   isReturning: true,
+   age: 26,
+   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-populateUser(!!you.isReturning, you.userName.firstName);
+console.log(you.userName)
+
+function populateUser(isReturning : boolean, name: string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = name
+}
+
+populateUser(!!you.isReturning, you.userName.firstName)
+
